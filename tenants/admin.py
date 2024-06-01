9b3b8dfcf83014f0e62595c56f4e8b82ac0e7bb5
mastrokostas <28702553+mastrokostas@users.noreply.github.com>
2024-06-01 17:20:35 +0300

    admin site moded

--- tenants/admin.py
+++ tenants/admin.py
@@ -5,6 +5,10 @@ from .models import Tenant
 # Register your models here.
 
 class TenantAdmin(admin.ModelAdmin):
-    list_display = ("last_name", "first_name", "tax_id",)
+    list_display = ("last_name", "first_name", "is_active", "is_blacklisted",)
 
 admin.site.register(Tenant, TenantAdmin)
+
+admin.site.site_header = "Prometheus Administration"
+admin.site.site_title = "CRM Administration"
+admin.site.site_title = "CRM Administration"
