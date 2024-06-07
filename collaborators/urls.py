fbc558ea0166c0d74499bf121d57e9768ac80b1b
mastrokostas <28702553+mastrokostas@users.noreply.github.com>
2024-06-07 21:53:50 +0300

    electricity completed

--- collaborators/urls.py
+++ collaborators/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('legal/', views.legal, name='legal'),
     path('edit_legal/<int:pk>/', views.edit_legal, name='edit_legal'),
     path('add_legal/', views.add_legal, name='add_legal'),
+    path('electricity/', views.electricity, name='electricity'),
+    path('edit_electricity/<int:pk>/', views.edit_electricity, name='edit_electricity'),
+    path('add_electricity/', views.add_electricity, name='add_electricity'),
+    
 ]
--- collaborators/views.py
+++ collaborators/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Legal
-from .forms import EditLegalForm
+from .models import Legal, Electricity
+from .forms import EditLegalForm, EditElectricityForm
 
 # Create your views here.
+
+
+## Legal
 
 @login_required(login_url='login')
 def all_collaborators(request):
@@ -35,3 +38,30 @@ def edit_legal(request,pk):
         messages.success(request, "Legal Team's information has been updated!")
         return redirect('legal')
     return render(request, 'collaborators/edit_legal.html', {'form': form, 'record': record})
+
+# Electricity
+
+@login_required(login_url='login')
+def electricity(request):
+    companies = Electricity.objects.all()
+    return render(request, "collaborators/electricity.html", {"companies":companies})
+
+@login_required(login_url='login')
+def add_electricity(request):
+    form = EditElectricityForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Electricity Company Added!")
+            return redirect('electricity')
+    return render(request, 'collaborators/add_electricity.html', {'form': form})
+
+@login_required(login_url='login')
+def edit_electricity(request,pk):
+    record = Electricity.objects.get(id=pk)
+    form = EditElectricityForm(request.POST or None, instance=record)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Electricity Company's information has been updated!")
+        return redirect('electricity')
+    return render(request, 'collaborators/edit_electricity.html', {'form': form, 'record': record})
