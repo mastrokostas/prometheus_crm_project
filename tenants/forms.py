72431358b13a98aef0fe68eb7dd9cf2798de8ced
mastrokostas <28702553+mastrokostas@users.noreply.github.com>
2024-06-08 16:46:57 +0300

    tenant forms completed

--- tenants/forms.py
+++ tenants/forms.py
@@ -15,18 +15,18 @@ NATIONALITY_CHOICES = {
 
 class AddTenantForm(forms.ModelForm):
     
-    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="*Required")
-    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="*Required")
-    fathers_name = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder":"Father's Name", "class":"form-control"}), label="")
-    phone = forms.IntegerField(required=False, widget=forms.widgets.NumberInput(attrs={"placeholder":"Phone Number", "class":"form-control"}), label="")
-    phone_2 = forms.IntegerField(required=False, widget=forms.widgets.NumberInput(attrs={"placeholder":"Phone Number 2", "class":"form-control"}), label="")
-    email = forms.EmailField(required=False, widget=forms.widgets.EmailInput(attrs={"placeholder":"email@domain.tdl", "class":"form-control"}), label="")
-    tax_id = forms.IntegerField(required=True, widget=forms.widgets.NumberInput(attrs={"placeholder":"Tax ID Number", "class":"form-control"}), label="*Required")
-    tax_office = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder":"Tax Office", "class":"form-control"}), label="")
-    id_type = forms.CharField(required=True, widget=forms.Select(attrs={"class":"form-control"}, choices=ID_CHOICES), label="Identification Type, *Required Choice")
-    id_number = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"ID or Passport Number", "class":"form-control"}), label="*Required")
-    nationality = forms.CharField(required=True, widget=forms.Select(attrs={"class":"form-control"}, choices=NATIONALITY_CHOICES), label="Nationality, *Required Choice")
-    notes = forms.CharField(required=False, widget=forms.Textarea(attrs={"placeholder":"Notes", "class":"form-control"}), label="")
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="First Name, *Required:")
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="Last Name, *Required:")
+    fathers_name = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder":"Father's Name", "class":"form-control"}), label="Father's Name:")
+    phone = forms.IntegerField(required=False, widget=forms.widgets.NumberInput(attrs={"placeholder":"Phone Number", "class":"form-control"}), label="Phone Number:")
+    phone_2 = forms.IntegerField(required=False, widget=forms.widgets.NumberInput(attrs={"placeholder":"Phone Number 2", "class":"form-control"}), label="Phone Number 2:")
+    email = forms.EmailField(required=False, widget=forms.widgets.EmailInput(attrs={"placeholder":"email@domain.tdl", "class":"form-control"}), label="Email:")
+    tax_id = forms.IntegerField(required=True, widget=forms.widgets.NumberInput(attrs={"placeholder":"Tax ID Number", "class":"form-control"}), label="Tax ID Number, *Required:")
+    tax_office = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder":"Tax Office", "class":"form-control"}), label="Tax Office:")
+    id_type = forms.CharField(required=True, widget=forms.Select(attrs={"class":"form-control"}, choices=ID_CHOICES), label="Identification Type, *Required Choice:")
+    id_number = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"ID or Passport Number", "class":"form-control"}), label="ID or Passport Number, *Required:")
+    nationality = forms.CharField(required=True, widget=forms.Select(attrs={"class":"form-control"}, choices=NATIONALITY_CHOICES), label="Nationality, *Required Choice:")
+    notes = forms.CharField(required=False, widget=forms.Textarea(attrs={"placeholder":"Notes for Tenant", "class":"form-control"}), label="Notes for Tenant:")
 
     class Meta:
         model = Tenant
@@ -36,21 +36,21 @@ class AddTenantForm(forms.ModelForm):
 
 
 class EditTenantForm(forms.ModelForm):
-    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="*Required")
-    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="*Required")
-    fathers_name = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder":"Father's Name", "class":"form-control"}), label="")
-    phone = forms.IntegerField(required=False, widget=forms.widgets.NumberInput(attrs={"placeholder":"Phone Number", "class":"form-control"}), label="")
-    phone_2 = forms.IntegerField(required=False, widget=forms.widgets.NumberInput(attrs={"placeholder":"Phone Number 2", "class":"form-control"}), label="")
-    email = forms.EmailField(required=False, widget=forms.widgets.EmailInput(attrs={"placeholder":"email@domain.tdl", "class":"form-control"}), label="")
-    tax_id = forms.IntegerField(required=True, widget=forms.widgets.NumberInput(attrs={"placeholder":"Tax ID Number", "class":"form-control"}), label="*Required")
-    tax_office = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder":"Tax Office", "class":"form-control"}), label="")
-    id_type = forms.CharField(required=True, widget=forms.Select(attrs={"class":"form-control"}, choices=ID_CHOICES), label="Identification Type *Required Choise:")
-    id_number = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"ID or Passport Number", "class":"form-control"}), label="*Required")
-    nationality = forms.CharField(required=True, widget=forms.Select(attrs={"class":"form-control"}, choices=NATIONALITY_CHOICES), label="Nationality, *Required Choise:")
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="First Name, *Required:")
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="Last Name, *Required:")
+    fathers_name = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder":"Father's Name", "class":"form-control"}), label="Father's Name:")
+    phone = forms.IntegerField(required=False, widget=forms.widgets.NumberInput(attrs={"placeholder":"Phone Number", "class":"form-control"}), label="Phone Number:")
+    phone_2 = forms.IntegerField(required=False, widget=forms.widgets.NumberInput(attrs={"placeholder":"Phone Number 2", "class":"form-control"}), label="Phone Number 2:")
+    email = forms.EmailField(required=False, widget=forms.widgets.EmailInput(attrs={"placeholder":"email@domain.tdl", "class":"form-control"}), label="Email:")
+    tax_id = forms.IntegerField(required=True, widget=forms.widgets.NumberInput(attrs={"placeholder":"Tax ID Number", "class":"form-control"}), label="Tax ID Number, *Required:")
+    tax_office = forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={"placeholder":"Tax Office", "class":"form-control"}), label="Tax Office:")
+    id_type = forms.CharField(required=True, widget=forms.Select(attrs={"class":"form-control"}, choices=ID_CHOICES), label="Identification Type, *Required Choice:")
+    id_number = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"ID or Passport Number", "class":"form-control"}), label="ID or Passport Number, *Required:")
+    nationality = forms.CharField(required=True, widget=forms.Select(attrs={"class":"form-control"}, choices=NATIONALITY_CHOICES), label="Nationality, *Required Choice:")
     is_active = forms.BooleanField(required=False)
     is_blacklisted = forms.BooleanField(required=False)
     legal_action = forms.BooleanField(required=False)
-    notes = forms.CharField(required=False, widget=forms.Textarea(attrs={"placeholder":"Notes", "class":"form-control"}), label="")
+    notes = forms.CharField(required=False, widget=forms.Textarea(attrs={"placeholder":"Notes for Tenant", "class":"form-control"}), label="Notes for Tenant:")
     
     class Meta:
         model = Tenant
