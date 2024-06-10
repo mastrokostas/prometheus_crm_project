2af77547546c3d474cf2786a0b0ba2c0dfc50063
mastrokostas <28702553+mastrokostas@users.noreply.github.com>
2024-06-10 17:37:49 +0300

    utilisation status auto updating on new rental agreement

--- ltrentals/views.py
+++ ltrentals/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from .models import RentalAgreement
 from .forms import AddRentalAgreement
 
+from properties.models import Property
+
 # Create your views here.
 
 @login_required(login_url='login')
@@ -21,6 +23,11 @@ def add_rental_agreement(request):
     form = AddRentalAgreement(request.POST or None, initial={'security_deposits':2})
     if request.method == 'POST':
         if form.is_valid():
+            #automate utilisation status
+            cleaned_property = form.cleaned_data.get('property')
+            tracked_property = Property.objects.get(id=cleaned_property.id)
+            tracked_property.utilisation_status = "Rented"
+            tracked_property.save()
             form.save()
             messages.success(request, "Rental Agreement Added!")
             return redirect('all_rental_agreements')
